bccf20774d3764d41432f254fb5c4af156ba7269
Ahmed <ahmed_l2@yahoo.com>
2024-08-01 22:35:47 +0300

    3. Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,11 +55,10 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a conenctor to the database"""
-    connector = mysql.connector.connection.MySQLConnection(
+    """Returns a connector to the database"""
+    return mysql.connector.connect(
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         database=os.getenv('PERSONAL_DATA_DB_NAME')
     )
-    return connector
