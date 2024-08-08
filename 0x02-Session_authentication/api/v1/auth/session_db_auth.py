8a275947e06d3f9e60a6324d2a7eb7cd4c3e0c7e
Ahmed <ahmed_l2@yahoo.com>
2024-08-08 22:57:47 +0300

    10. Sessions in database

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Module for the SessionDBAuth class"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from models.base import Base
+
+
+class SessionDBAuth(SessionExpAuth):
+    """DB Session Authentication that inherits from SessionExpAuth"""
+
+    def create_session(self, user_id=None):
+        """
+        Creates and stores a new instance of UserSession
+        and returns the Session ID
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        Returns the User ID by requesting UserSession in the database
+        based on session_id
+        """
+        if session_id is None:
+            return None
+
+        user_session = UserSession.query.filter_by(
+            session_id=session_id).first()
+        if user_session is None:
+            return None
+
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """
+        Destroys the UserSession based on the Session ID
+        from the request cookie
+        """
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_session = UserSession.query.filter_by(
+            session_id=session_id).first()
+        if user_session is None:
+            return None
+        user_session.delete()
+
+        return True
