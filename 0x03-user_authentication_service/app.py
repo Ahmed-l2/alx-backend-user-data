6d6445904b6509884b6df498cc1c38cd2c4b9685
Ahmed <ahmed_l2@yahoo.com>
2024-08-14 21:45:11 +0300

    15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,5 +50,14 @@ def logout():
     return redirect(url_for('main'))
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Retrieves user email"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
